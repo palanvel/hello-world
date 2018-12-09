77bde6736b93acf74c1c94704788622ab5e9005c
palanivel <palanvel@gmail.com>
2018-12-09 16:11:01 +0000

    add the comment added file

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ class HelloWorld
 { 
     // Your program begins with a call to main(). 
     // Prints "Hello, World" to the terminal window. 
+    //hi welcom
     public static void main(String args[]) 
     { 
         System.out.println("Hello, World"); 
